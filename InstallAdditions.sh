66d6b3893a72c2118d08f8791919852b1c96cfa2
Mauznemo <mauznemo@gmail.com>
2024-07-28 15:35:35 +0200

    refactor: change wording and apt output

--- InstallAdditions.sh
+++ InstallAdditions.sh
@@ -6,7 +6,7 @@ function installCameraPatch()
 {
     echo "Installing dependencies..."
 
-    sudo apt install -y ffmpeg v4l2loopback-dkms > /dev/null 2>&1
+    sudo apt install -y ffmpeg v4l2loopback-dkms #> /dev/null 2>&1
 
     echo "Installing camera patch..."
     local destinationDir="$HOME/"
@@ -35,7 +35,7 @@ items=(
 
 printf "\n\n\n"
 
-echo -e "${BOLD}Please select one or more additional options to install (separated by space):${RESET}"
+echo -e "${BOLD}Please select additional options to install (separated by space, Enter for none):${RESET}"
 for i in "${!items[@]}"; do
     if [[ $((i % 2)) -eq 0 ]]; then
         printf "%s. %s\n" "${items[$i]}" "${items[$((i + 1))]}"
