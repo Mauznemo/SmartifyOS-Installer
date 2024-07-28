7ebed37bacdcc1408a8000c72d545966d8a18953
Mauznemo <mauznemo@gmail.com>
2024-07-28 15:31:26 +0200

    fix: correct service name

--- SmartifyOS/Scripts/SetNetworkServices.sh
+++ SmartifyOS/Scripts/SetNetworkServices.sh
@@ -9,10 +9,10 @@ fi
 
 if [ "$ENABLE" = "enable" ]; then
     sudo systemctl enable NetworkManager.service
-    sudo systemctl enable networking.service
+    sudo systemctl enable networkd-dispatcher.service
     sudo systemctl enable wpa_supplicant.service
 else
     sudo systemctl disable NetworkManager.service
-    sudo systemctl disable networking.service
+    sudo systemctl disable networkd-dispatcher.service
     sudo systemctl disable wpa_supplicant.service
 fi
