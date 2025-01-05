df741e9db3f7d985467dd32c4ecf84f353aa41c2
Mauznemo <mauznemo@gmail.com>
2025-01-05 01:42:59 +0100

    refactor: fullscreen Android Auto faster

--- Additionals/AndroidAuto/SmartifyOS/AndroidAuto/start-android-auto.sh
+++ Additionals/AndroidAuto/SmartifyOS/AndroidAuto/start-android-auto.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-./fullscreen-app.sh 10 "desktop-head-unit" &
+./fullscreen-app.sh 2 "desktop-head-unit" &
 
 #sudo apt install adb libc++1 libc++abi1
 ./desktop-head-unit -u -c "config/default.ini"
