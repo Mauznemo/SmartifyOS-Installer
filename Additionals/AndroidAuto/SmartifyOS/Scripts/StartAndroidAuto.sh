1a7bcd6807f924c2c11fef54ab9c74f9a4ecef69
Mauznemo <mauznemo@gmail.com>
2025-01-16 21:21:21 +0100

    fix: AA not starting

--- Additionals/AndroidAuto/SmartifyOS/Scripts/StartAndroidAuto.sh
+++ Additionals/AndroidAuto/SmartifyOS/Scripts/StartAndroidAuto.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-xdotool search --name "StartAndroidAuto" windowminimize
+#xdotool search --name "StartAndroidAuto" windowminimize //This sometimes stops the script so removed for now
 
 cd ~/SmartifyOS/AndroidAuto/ || exit
 
